7bc5529c0056b0ee675f7ef8dcf16f672f1bbdfd
Vanderley Rocha <vanderley@bol.com.br>
2026-01-07 11:04:35 -0500

    wsl ubuntu

--- database/migrations/2025_07_18_171910_create_alunos_table.php
+++ database/migrations/2025_07_18_171910_create_alunos_table.php
@@ -11,35 +11,7 @@ return new class extends Migration
         Schema::create('alunos', function (Blueprint $table) {
             $table->collation = 'utf8mb4_0900_ai_ci';
             $table->id('Id');
-            $table->boolean('registro_unico')->nullable();
-            $table->integer('prioridade')->default(10);
-            $table->bigInteger('cod_inep_aluno')->nullable();
-            $table->string('cpf', 11)->nullable();
-            $table->string('nome', 190)->nullable();
-            $table->date('dt_nascimento')->nullable();
-            $table->string('cor', 100)->nullable();
-            $table->string('sexo', 40)->nullable();
-            $table->integer('ano_censo')->nullable();
-            $table->bigInteger('cod_inep_escola')->nullable();
-            $table->string('municipio', 100)->nullable();
-            $table->string('uf', 2)->nullable();
-            $table->string('escola', 100)->nullable();
-            $table->string('modalidade', 100)->nullable();
-            $table->string('etapa', 100)->nullable();
-            $table->bigInteger('cod_turma')->nullable();
-            $table->string('nome_turma', 100)->nullable();
-            $table->string('estrutura_curricular', 100)->nullable();
-            $table->string('tipo_mediacao', 100)->nullable();
-            $table->string('tipo_atendimento', 100)->nullable();
-            $table->string('localizacao', 100)->nullable();
-            $table->string('dependencia', 100)->nullable();
-            $table->string('local_funcionamento_turma', 100)->nullable();
-            $table->string('dias_semana', 100)->nullable();
-            $table->string('horario', 100)->nullable();
-            $table->text('forma_organizacao')->nullable();
-            $table->boolean('libras')->nullable();
-            $table->string('deficiencia', 190)->nullable();
-            $table->text('recursos')->nullable();
+
             $table->timestamps();
         });
     }
